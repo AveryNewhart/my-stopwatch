4050c44890bcc285f88981c956e98abe0fce1627
AveryNewhart <newhartreeceavery@gmail.com>
2023-07-31 18:25:03 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <div>00:00</div>
+    <button>Start</button>
+    <button>Pause</button>
+    <button>Restart</button>
+  </div>
+</template>
+
+<style scoped>
+</style>
